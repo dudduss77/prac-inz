8284f9ee70eded86be17fb6e664816d694d81f07
Damian <damiankarbowiak17@gmail.com>
2021-11-08 22:26:36 +0100

    Button edit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import styled from 'styled-components';
-import Theme from './Theme';
-import './fontConfig'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Button from './components/Button';
+import styled from "styled-components";
+import Theme from "./Theme";
+import "./fontConfig";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "./components/Button";
 
 const Test = styled.div`
   width: 100px;
   height: 100px;
-  background: ${props => props.theme.naturalOne};
-`
+  background: ${(props) => props.theme.naturalOne};
+`;
 
 const TestTwo = styled.div`
   width: 100px;
   height: 100px;
-  background: ${props => props.theme.naturalTwo};
-`
+  background: ${(props) => props.theme.naturalTwo};
+`;
 
 function App() {
   return (
     <Theme>
-      <Test/>
-      <TestTwo/>
-      <FontAwesomeIcon icon="plus"/>
+      <Test />
+      <TestTwo />
+      <FontAwesomeIcon icon="plus" />
       <Button>Rejestracja</Button>
       <Button isWholeContent>Rejestracja</Button>
-      <Button pm="5">Rejestracja</Button>
-      <Button br="20">Rejestracja</Button>
+      <Button pTB="5">Rejestracja</Button>
+      <Button isRounded>Rejestracja</Button>
+      <Button>
+        <FontAwesomeIcon icon="plus" /> Dodaj nowego
+      </Button>
+      <Button>
+        <FontAwesomeIcon icon="plus" /> 
+      </Button>
     </Theme>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,17 @@ import styled from "styled-components";
 
 const Button = styled.button`
   border: none;
-  border-radius: ${props => props.br ? props.br + "px" : "2px"};
-  background: ${props => props.theme.PrimarySix};
-  color: ${props => props.theme.CharacterPrimaryInvers};
-  padding: ${props => props.pm ? props.pm + 'px' : '10px'};
+  border-radius: ${(props) => (props.isRounded ? "50px" : "2px")};
+  background: ${(props) => props.theme.PrimarySix};
+  color: ${(props) => props.theme.CharacterPrimaryInvers};
+  padding: ${(props) => (props.pTB ? props.pTB + "px" : "10px")} 20px;
   font-size: 16px;
-  width: ${props => props.isWholeContent ? '100%' : 'auto'};
+  width: ${(props) => (props.isWholeContent ? "100%" : "auto")};
   cursor: pointer;
-`
+
+  &:hover {
+    filter: brightness(95%);
+  }
+`;
 
 export default Button;
