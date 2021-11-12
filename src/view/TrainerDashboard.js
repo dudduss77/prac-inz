ff326c9f215c817c12dfbfb834f1872d0993da0c
Jarek <biniox2@gmail.com>
2021-11-12 16:35:20 +0100

    input component

--- src/view/TrainerDashboard.js
+++ src/view/TrainerDashboard.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import Checkbox from "../components/Checkbox";
+import Input from "../components/Input";
 import { ReusableViewWrapper } from "../components/Reusable";
+
+
+import { ReactComponent as UserSVG } from './../assets/user.svg';
+import { ReactComponent as passSVG } from './../assets/pass.svg';
 
 const TestDiv = styled.div`
   width: 250px;
@@ -12,13 +17,16 @@ const TestDiv = styled.div`
 const TrainerDashboard = () => {
   return (
     <ReusableViewWrapper isColumnLayout={true}>
-      <Checkbox label="test" disabled/>
+      <Input placeholder="inputs" icon={UserSVG} width="360px" />  
+      <Input placeholder="inputs" icon={passSVG} />  
+      <Input placeholder="inputs" />  
+      {/* <Checkbox label="test" disabled/>
       <Checkbox label="test"/>
       <Checkbox label="Testowy z większym label"/>
       <TestDiv />
       <TestDiv />
       <TestDiv />
-      <TestDiv />
+      <TestDiv /> */}
     </ReusableViewWrapper>
   );
 };
