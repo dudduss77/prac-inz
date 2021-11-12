cc19429b0a8c6efe6fbbbe08e3a2fb1002afbb57
Jarek <biniox2@gmail.com>
2021-11-12 20:46:07 +0100

    change input in Protege Modal

--- src/components/Modal/components/NewProtege.js
+++ src/components/Modal/components/NewProtege.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Button from "../../Button";
+import Input from "../../Input";
 import { ModalHeader, ModalParagraph } from "./ModalReusable";
+
+import { ReactComponent as UserSVG } from './../../../assets/user.svg';
 
 const NewProtege = () => {
   return (
     <>
       <ModalHeader>Nowy podopieczny</ModalHeader>
-      <input type="text" />
+      <Input placeholder="Email" icon={UserSVG}/>
       <ModalParagraph align="center">
         Podopieczny otrzyma prośbę o wypełnienie ankiety wstępnej. Jeśli nie
         stworzyłeś jeszcze ankiety, zrób to przed dodaniem podopiecznego. W
