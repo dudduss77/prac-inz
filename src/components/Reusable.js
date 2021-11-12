38fac9b9a6eeab854bc260814fd64e165423d119
Damian Karbowiak <damiankarbowiak17@gmail.com>
2021-11-12 14:44:23 +0100

    Dashboard messages, Box, HeaderBox

--- src/components/Reusable.js
+++ src/components/Reusable.js
@@ -5,4 +5,37 @@ export const ReusableViewWrapper = styled.div`
   flex-direction: ${({ isColumnLayout }) =>
     isColumnLayout ? "column" : "row"};
   gap: 10px;
+  flex-wrap: wrap;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  gap: 10px;
+
+  @media screen and (max-width: 900px) {
+    flex-direction: column;
+  }
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Box = styled.div`
+  width: ${({ width }) => width};
+  min-width: ${({ minWidth }) => minWidth};
+  max-width: ${({ maxWidth }) => maxWidth};
+  display: flex;
+  flex-direction: column;
+  gap: ${({ isGap }) => (isGap ? "10px" : "0")};
+  background: ${({ theme }) => theme.naturalOne};
+  height: ${({ height }) => height};
+  min-height: ${({ minHeight }) => minHeight};
+  max-height: ${({ maxHeight }) => maxHeight};
+
+  @media screen and (max-width: 900px) {
+    width: 100%;
+  }
 `;
