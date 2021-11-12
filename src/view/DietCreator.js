59bc48f288b5a5fc7986ea2b6995ce255959008e
Jarek <biniox2@gmail.com>
2021-11-12 16:41:27 +0100

    .

--- src/view/DietCreator.js
+++ src/view/DietCreator.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { ReusableViewWrapper } from '../components/Reusable'
+import Input from "../components/Input";
+
+import { ReactComponent as UserSVG } from './../assets/user.svg';
+import { ReactComponent as passSVG } from './../assets/pass.svg';
 
 const DietCreator = () => {
   return (
     <ReusableViewWrapper>
       DietCreator
+      <Input placeholder="inputs" icon={UserSVG} width="360px" />  
+      <Input placeholder="inputs" icon={passSVG} />  
+      <Input placeholder="inputs" />  
     </ReusableViewWrapper>
   )
 }
