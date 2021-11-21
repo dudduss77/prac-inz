7eb22f4771b7c5936efd3f24edf40a256167cc80
Jarosław Bińczyk <biniox2@gmail.com>
2021-11-21 18:53:13 +0100

    .

--- src/components/Modal/components/NewCalendar.js
+++ src/components/Modal/components/NewCalendar.js
@@ -6,6 +6,7 @@ import { changeModalState } from "../../../features/AppSlice";
 import { Button, Row } from "../../Reusable";
 import { ModalHeader } from "./ModalReusable";
 import Select from './../../../components/Select'
+import Input from "../../Input";
 
 const RedButton = styled(Button)`
   background: ${({ theme }) => theme.DustRedSeven};
@@ -17,19 +18,35 @@ const NewCalendar = () => {
 
   const deleteDiet = () => {
     modalDispatch(changeModalState())
-    navigate('/diet')
   }
 
   return (
     <>
       <ModalHeader>Nowy wpis w kalendarzu</ModalHeader>
 
-      <Row>
-          <Select data={["1"]} />
-      </Row>
+        <Select 
+            data={["Poniedziałek 16.01.21", "Poniedziałek 16.01.21", "Poniedziałek 16.01.21"]} 
+            width="360px"
+            placeholder="Data"
+        />
+        <Row isGap >
+            <Input placeholder="10:00" width="50px" paddingLeft="5px" />
+            <Input placeholder="10:00" width="50px" paddingLeft="5px" />
+        </Row>
+        <Select 
+            data={[
+                "Zwykły wpis", 
+                "Widoczne dla podopiecznych jako do rezerwacji", 
+                "Trening lub spotkanie z podopiecznym"
+            ]} 
+            width="360px"
+            placeholder="Zwykły wpis"
+        />
+        <Input placeholder="Nazwa" />
+        <Input as="textarea" placeholder="Opis..." height="100px" />
       <Row isGap noMedia>
         <RedButton onClick={() => deleteDiet()}>Zapisz</RedButton>
-        <Button onClick={() => modalDispatch(changeModalState())}>Usuń</Button>
+        <Button onClick={() => {}}>Usuń</Button>
       </Row>
     </>
   );
